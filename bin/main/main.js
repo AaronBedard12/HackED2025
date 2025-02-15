8048c477704200f5c5cf1ce21cf54d2eda549b11
“Aaron <sirorangepeel@gmail.com>
2025-02-15 15:29:40 -0700

    Small edits to main.html/js

--- bin/main/main.js
+++ bin/main/main.js
@@ -1,7 +1,7 @@
 document.getElementById("open-side-panel").addEventListener("click", () => {
-    // Use the Chrome API to open the side panel
-    chrome.windows.getCurrent((window) => {
-      chrome.sidePanel.open({ windowId: window.id });
-    });
-    window.close();
+  // Use the Chrome API to open the side panel
+  chrome.windows.getCurrent((window) => {
+    chrome.sidePanel.open({ windowId: window.id });
   });
+  window.close(); // Close the current popup
+});
