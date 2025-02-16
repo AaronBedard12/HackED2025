2fc06acca567a8628a2abebb1057a3fa94fe960b
“Aaron <sirorangepeel@gmail.com>
2025-02-16 14:30:12 -0700

    Main flow finished

--- bin/main/main.js
+++ bin/main/main.js
@@ -1,6 +1,10 @@
 var selectedValue;
 let hasSelection = false;
 
+function OnBegin() {
+  document.getElementById("quickmenu").style.display = "none";
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM fully loaded!");
 
@@ -15,9 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Attach event listeners when the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function() {
-  // Attach event listener to the "Submit and Quit" button
-  document.getElementById("submitAndQuit").addEventListener("click", SubmitAndQuit);
-
   // Attach event listener to the "Submit" button
   document.getElementById("submit").addEventListener("click", submit);
 });
@@ -26,7 +27,18 @@ document.addEventListener("DOMContentLoaded", function() {
 function submit() {
   selectedValue = $('input[name="WellnessCheckOption"]:checked').val();
   console.log("Selected value:", selectedValue);
+  document.getElementById("quickmenu").style.display = "block";
+  document.getElementById("popup").style.display = "none";
   SaveValue();
   hasSelection = true;
 }
 
+function SaveValue() {
+  // Save the selected value in chrome.storage
+  const key = 'selectedValue_' + Date.now(); // Use Date.now() for a unique key
+  chrome.storage.local.set({ [key]: selectedValue }, function() {
+    console.log('Value saved:', selectedValue);
+  });
+}
+
+window.addEventListener("load", OnBegin);
