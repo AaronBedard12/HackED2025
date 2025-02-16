e39ec1a348fca22f556358c1c32c308c8fe4eada
“Aaron <sirorangepeel@gmail.com>
2025-02-16 14:06:29 -0700

    Merging main

--- bin/main/main.js
+++ bin/main/main.js
@@ -1,3 +1,6 @@
+var selectedValue;
+let hasSelection = false;
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM fully loaded!");
 
@@ -9,3 +12,21 @@ document.addEventListener("DOMContentLoaded", () => {
     window.close(); // Close the current popup
   });
 });
+
+// Attach event listeners when the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function() {
+  // Attach event listener to the "Submit and Quit" button
+  document.getElementById("submitAndQuit").addEventListener("click", SubmitAndQuit);
+
+  // Attach event listener to the "Submit" button
+  document.getElementById("submit").addEventListener("click", submit);
+});
+
+
+function submit() {
+  selectedValue = $('input[name="WellnessCheckOption"]:checked').val();
+  console.log("Selected value:", selectedValue);
+  SaveValue();
+  hasSelection = true;
+}
+
